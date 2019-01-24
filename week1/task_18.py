79100da98b5f7bab5728d435a69f0db6a168419a
Sergey Prokopenko <prokopenko@t8.ru>
2019-01-24 10:03:06 +0300

    fixed task_18 week1

--- week1/task_18.py
+++ week1/task_18.py
@@ -2,11 +2,8 @@ n = int(input()) % 10000001
 sec = 60*60*24
 time = n % sec
 h = time // (60 * 60)
-print(h)
 time = time - h * 60 * 60
 m = time // 60
-print(m)
 time = time - m * 60
 s = time
-print(s)
 print(h, ':', m // 10, m % 10, ':', s // 10, s % 10, sep='')
